4496f5bb4732bfd8b6ad48ab35c5493554ef95d2
Skwardlow <mananasasai@gmail.com>
2018-02-13 02:10:48 +0700

    first v

---
+++ hwrd.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+   printf("Hello, world! \n");
+   return 0;
+std::cin.ignore();
+std::cin.get();
+//ya zabil pro nescol'ko etapov. prostite. nadeyus, tak sojdet?
+}
